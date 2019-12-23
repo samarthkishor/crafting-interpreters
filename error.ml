85a20f64f193547acfa0001bdbe419eb4670549d
Samarth Kishor <samarthkishor1@gmail.com>
2019-12-23 14:01:37 -0500

    Add necessary value and error types for the interpreter

--- error.ml
+++ error.ml
@@ -2,14 +2,39 @@ type runtime_error =
   { where : int
   ; message : string }
 
+type type_error =
+  { observed_type : Value.eval_type
+  ; expected_type : Value.eval_type }
+
+type error =
+  | RuntimeError of runtime_error
+  | TypeError of type_error
+
 exception RuntimeError of runtime_error
+exception TypeError of type_error
 
 let had_error = ref false
+let had_runtime_error = ref false
 
 let report line where message =
   Printf.eprintf "[line %d] Error %s: %s\n" line where message;
   flush stderr;
   had_error := true
+;;
+
+let report_runtime_error (error : error) =
+  match error with
+  | TypeError e ->
+    Printf.eprintf
+      "TypeError: observed type %s but expected type %s\n"
+      (Value.string_of_eval_type e.observed_type)
+      (Value.string_of_eval_type e.expected_type);
+    flush stderr;
+    had_runtime_error := true
+  | RuntimeError e ->
+    Printf.eprintf "[line %d] RuntimeError: %s\n" e.where e.message;
+    flush stderr;
+    had_runtime_error := true
 ;;
 
 let error line message = report line "" message
--- value.ml
+++ value.ml
@@ -5,6 +5,12 @@ type t =
   | LoxString of string
   | LoxNil
 
+type eval_type =
+  | Bool
+  | Number
+  | String
+  | Nil
+
 let string_of value =
   match value with
   | LoxBool b -> string_of_bool b
@@ -12,4 +18,20 @@ let string_of value =
   | LoxNumber n -> Printf.sprintf "%g" n
   | LoxString s -> s
   | LoxNil -> "nil"
+;;
+
+let string_of_eval_type eval_type =
+  match eval_type with
+  | Bool -> "Bool"
+  | Number -> "Number"
+  | String -> "String"
+  | Nil -> "Nil"
+;;
+
+let type_of value =
+  match value with
+  | LoxBool _ -> Bool
+  | LoxNumber _ | LoxInt _ -> Number
+  | LoxString _ -> String
+  | LoxNil -> Nil
 ;;
