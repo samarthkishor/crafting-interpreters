824c036be7b88717c0e1eb3262919726b0fd20ba
Samarth Kishor <samarthkishor1@gmail.com>
2020-01-10 14:55:28 -0500

    Autoformat Environment module

--- lib/environment.ml
+++ lib/environment.ml
@@ -1,8 +1,9 @@
 type t =
   { values : (string, Value.t) Hashtbl.t
-  ; mutable enclosing : t option }
+  ; mutable enclosing : t option
+  }
 
-let init ?enclosing () = {values = Hashtbl.create 32; enclosing}
+let init ?enclosing () = { values = Hashtbl.create 32; enclosing }
 
 let rec get_value environment (name : Scanner.token) =
   match Hashtbl.find_opt environment.values name.lexeme with
@@ -11,7 +12,7 @@ let rec get_value environment (name : Scanner.token) =
     | None ->
       raise
       @@ Error.RuntimeError
-           {where = name.line; message = "Undefined variable '" ^ name.lexeme ^ "'."}
+           { where = name.line; message = "Undefined variable '" ^ name.lexeme ^ "'." }
     | Some enclosing -> get_value enclosing name)
   | Some value -> value
 ;;
@@ -58,7 +59,7 @@ let rec assign environment (name : Scanner.token) value =
     | None ->
       raise
       @@ Error.RuntimeError
-           {where = name.line; message = "Undefined variable '" ^ name.lexeme ^ "'."}
+           { where = name.line; message = "Undefined variable '" ^ name.lexeme ^ "'." }
     | Some enclosing -> assign enclosing name value)
   | Some _ ->
     Hashtbl.replace environment.values name.lexeme value;
